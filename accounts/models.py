84318b4c37f7779508c3336aea552506660cbb56
RahulPrajapat <rickey26photon@gmail.com>
2020-04-27 05:26:21 +0100

    added user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=10, min_length=6)
+
