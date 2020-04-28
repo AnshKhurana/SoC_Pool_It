0a22f3fa9baa586478ecc3147970828b27ecde78
RahulPrajapat <rickey26photon@gmail.com>
2020-04-28 07:16:30 +0100

    created a gitignore file

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,5 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class User(models.Model):
-    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
-    password = models.CharField(max_length=10, min_length=6)
-
