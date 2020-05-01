0819acd94f7560432070a7acbcf7030d33b83dfd
RahulPrajapat <rickey26photon@gmail.com>
2020-05-01 11:51:19 +0100

    updated models

--- main/models.py
+++ main/models.py
@@ -1,25 +1,42 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import User, auth
-
+from django.contrib.auth.models import auth
+from accounts.models import User
 # Create your models here.
 
 #---------------------------------------------------#
 
 class service(models.Model):
     service_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    group_id = models.ForeignKey(group, on_delete=models.CASCADE)
     service_type_id = models.ForeignKey()
-    initiator_id = models.ForeignKey(User)
+    initiator_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     service_desc = TextField()
-    start_time = models.DateTimeField()
+    start_time = models.DateTimeField(null=True)
     slackness = models.TimeField()
+    members = models.ManyToManyField(User,
+                                        through='service_member',
+                                        through_fields=('service_id', 'user_id')
+                                        )
+    groups = models.ManyToManyField(group,
+                                        through='service_group',
+                                        through_fields=('service_id', 'group_id'),
+                                        )
+
+    def __str__(self):
+        return self.service_desc
 
 class shoping(service):
 
 class food(service):
 
 class travel(service):
+    start_time = 
+    end_point = 
+    travel_choice = [
+
+    ]
+    transport =
+
 
 class event(service):
 
@@ -27,9 +44,16 @@ class event(service):
 
 class group(models.Model):
     group_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    admin = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    members = models.ManyToManyField(User, 
+                                        through='group_member', 
+                                        through_fields=('group_id', 'user_id')
+                                    )
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+
 
 class group_member(models.Model):
-    room = models.ManyToManyField(group)
     group_id = models.ForeignKey(group, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -47,3 +71,11 @@ class Message(models.Model):
 #---------------------------------------------------#
 
 class service_group(models.Model):
+    service_id = models.ForeignKey(service, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(group, on_delete=models.CASCADE)
+
+class service_member(models.Model):
+    service_id = models.ForeignKey(service, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#---------------------------------------------------#
