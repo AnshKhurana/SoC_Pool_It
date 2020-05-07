879f76a6ba2679aa32085a862e3041c8d79c8a7d
Mukesh Reddy <julakantimukeshreddy123@gmail.com>
2020-05-07 15:50:36 +0530

    Changed first form into django form

--- services/forms.py
+++ services/forms.py
@@ -1,4 +1,20 @@
 from django import forms
-from main.models import group,FoodService
+from main.models import group,Category
 from accounts.models import User
+from django.forms.widgets import CheckboxSelectMultiple 
 
+class groupsform(forms.Form):
+
+	def __init__(self,*args,**kwargs):
+		user=kwargs.pop('user',None)
+		super().__init__(*args,**kwargs)
+		self.fields['groups_list'].queryset=group.objects.filter(members=user)
+
+	groups_list=forms.ModelMultipleChoiceField(queryset=None,required=True,label='Select groups to post the service:',widget=CheckboxSelectMultiple)
+
+	category=forms.ModelChoiceField(queryset=Category.objects.all(),required=True,label='Choose the Category:',initial=Category.objects.get(pk=1))
+
+
+
+
+#class FoodServiceForm(forms.ModelForm):
--- services/views.py
+++ services/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render,redirect
-from accounts.models import User
 from main.models import group
+from services.forms import groupsform
 
 	
 def groupservice(request):
-	user=request.user
-	group_list=group.objects.filter(members=user)
-	return render(request,'servicegroups.html',{'group_list':group_list})
+	form=groupsform(user=request.user)
+	return render(request,'servicegroups.html',{'form':form})
 
+def serviceinfo(request):
+	if request.method=='POST':
+		request.session['servicegroups']=request.POST['groups_list']
+		if request.POST['category']=='FoodService':
+
+
+		if request.POST['category']=='EventService':
+
+
+		if request.POST['category']=='ShoppingService':
+
+		
+		if request.POST['category']=='TravelService':
