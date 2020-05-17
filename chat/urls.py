7e8a839fe04ca9cf5de839cfdbdf12fc2f38f778
megha-baboria <190050067@iitb.ac.in>
2020-05-17 15:05:13 +0530

    added restrictions

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-
+app_name='chat'
 urlpatterns = [
     path('<s_id>/create', views.create_message_view, name='create'),
    
--- chat/views.py
+++ chat/views.py
@@ -4,22 +4,34 @@ from accounts.models import User
 from chat.forms import ChatForm
 from django.utils import timezone
 from main.models import service
+from django.http import HttpResponse
 
 # Create your views here.
-def create_message_view(request,s_id = '0'):
+def create_message_view(request,s_id ):
     form_class = ChatForm
     form = form_class(request.POST or None)
     qs = Message1.objects.all()
+    service_obj = get_object_or_404(service , service_id = s_id)
+
     if request.method=='POST':
-        if form.is_valid():
-            message = form.save(commit=False)
-            message.user = request.user
-            message.service = get_object_or_404(service , service_id = s_id)
-            message.timestamp = timezone.now()
-            message.save()
-            qs = Message1.objects.all()
-            form = ChatForm()
-    return render (request,'message_create.html',{"form" : form,"obj_list": qs })
+        
+        message = form.save(commit=False)
+        message.user = request.user
+        message.service = service_obj
+        message.timestamp = timezone.now()
+        message.save()
+        print(qs)
+        form = ChatForm()
+        str ='/chat/'+s_id+'/create'
+        return redirect(str)
+        
+            
+    else:
+        if request.user in service_obj.members.all():
+            qs = Message1.objects.filter(service = service_obj)
+            return render (request,'message_create.html',{"form" : form,"obj_list": qs })
+        else:
+            return HttpResponse ('You have to be a member of the service to join the chat')
 
 
 
