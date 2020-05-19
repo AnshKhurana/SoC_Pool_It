5b4a0f959fc432c498f0be2cdff3ad20b1c1cbcf
megha-baboria <190050067@iitb.ac.in>
2020-05-19 19:48:19 +0530

    Impoved templates

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,16 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import random
+def random_string():
+    return str(random.randint(0,255))
 
 # Create your models here.
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     mobile = models.IntegerField(null=True)
+    rn1=models.CharField(max_length=3,default = random_string)
+    rn2=models.CharField(max_length=3,default = random_string)
+    rn3=models.CharField(max_length=3,default = random_string)
+
