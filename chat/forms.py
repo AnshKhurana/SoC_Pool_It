7cd65d13691390cc7a3c4fded0e21ca87e81169c
megha-baboria <190050067@iitb.ac.in>
2020-05-21 19:47:52 +0530

    fixed ajax

--- chat/forms.py
+++ chat/forms.py
@@ -2,18 +2,18 @@
 from django import forms
 from main.models import Message
 from accounts.models import User
-
+from django.contrib.admin import widgets
 
 
 class ChatForm(forms.ModelForm):
 
 	class Meta:
-		model = Message
-		fields = ['content']
+		model=Message
+		fields=['content']
 		widgets = {
-			'text': forms.TextInput(attrs={
-				'id': 'post-message',
-				'required': True,
-				'placeholder': 'Type your message...',
-			}),
-		}
+            'content': forms.TextInput(attrs={
+                'id': 'post-text', 
+                'required': True, 
+                'placeholder': 'Say something...'
+            }),
+        }
