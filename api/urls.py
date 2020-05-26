9e9a4af6381e84a7d80389996bc3db8dacd27a1b
Mukesh Reddy <julakantimukeshreddy123@gmail.com>
2020-05-26 19:18:37 +0530

    modified api add added some js

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,5 @@ from .views import *
 
 urlpatterns=[
 	path('servicefilter/',servicefilterview.as_view(),name='servicefilter'),
-	path('searchfilter/',searchview.as_view(),name='searchfilter'),
 	path('addservicemember/',add_service_member,name='add_service_member'),
 ]
---
+++ static/js/services.js
@@ -0,0 +1,137 @@
+
+function ServiceFiltering(){
+	var select=document.getElementsById('services_available');
+	select.innerHTML="";
+	axios.get('/api/servicefilter',{
+		params:{
+			service=service,
+			group_ids=group_ids_string,
+			start_time=start_time,
+			end_time=end_time,
+			text=text,
+		}
+
+	}).then(
+		function(response){
+			reply=response.data;
+			console.log(reply);
+
+			for (item in reply)
+			{
+				if(reply[item].is_active==true){
+					if(reply[item].service_type in ['Food','Shopping']){
+						if(reply[item].is_member==true){
+							select.innerHTML += "<li> <div>" +
+							"<h4>"reply[item].service_type+" Service</h4>" +
+				 			"<h5>" + reply[item].vendor + "</h5>" +
+							"<p> by " + reply[item].initiator + "</p>" +
+							"<h4><p>Start Time: " + reply[item].start_time + "<br> End Time: " + reply[item].end_time + "</p> </h4>" +
+							"<span> " + reply[item].service_desc + "</span>" +
+							"<button >Already a member</button>"
+							+ " </div> </li>";
+						}
+
+						else{
+							{#here, reply[item] is our service object, can access fields directly,#}
+							select.innerHTML += "<li> <div>" +
+							"<h4>"reply[item].service_type+" Service</h4>" +
+							"<h5>" + reply[item].vendor + "</h5>" +
+							"<p> by " + reply[item].initiator + "</p>" +
+							"<h4><p>Start Time: " + reply[item].start_time + "<br> End Time: " + reply[item].end_time + "</p> </h4>" +
+							"<span> " + reply[item].service_desc + "</span>" +
+							"<button onclick=\"join_service(" + reply[item].service_id + ")\" id=\"" + reply[item].service_id + "\">Join Service</button>"
+							+ "</div> </li>";
+						}
+					}
+
+					if(reply[item].service_type=='Event'){
+						if(reply[item].is_member==true){
+							select.innerHTML += "<li><div>" +
+							"<h4>Event Service</h4>" +
+				 			"<h5>" + reply[item].event_type + "</h5>" +
+				 			"<h5>" + reply[item].location + "</h5>"
+							"<p> by " + reply[item].initiator + "</p>" +
+							"<h4><p>Start Time: " + reply[item].start_time + "<br> End Time: " + reply[item].end_time + "</p> </h4>" +
+							"<span> " + reply[item].service_desc + "</span>" +
+							"<button >Already a member</button>"
+							+ "</div> </li>";
+						}
+
+						else{
+							{#here, reply[item] is our service object, can access fields directly,#}
+							select.innerHTML += "<li><div>" +
+							"<h4>Event Service</h4>" +
+							"<h5>" + reply[item].event_type + "</h5>" +
+				 			"<h5>" + reply[item].location + "</h5>"
+							"<p> by " + reply[item].initiator + "</p>" +
+							"<h4><p>Start Time: " + reply[item].start_time + "<br> End Time: " + reply[item].end_time + "</p> </h4>" +
+							"<span> " + reply[item].service_desc + "</span>" +
+							"<button onclick=\"join_service(" + reply[item].service_id + ")\" id=\"" + reply[item].service_id + "\">Join Service</button>"
+							+ "</div> </li>";
+						}
+					}
+
+					if(reply[item].service_type=='Travel'){
+						if(reply[item].is_member==true){
+							select.innerHTML += "<li><div>" +
+							"<h4>Shopping Service</h4>" +
+				 			"<h5>" + reply[item].travel + "</h5>" +
+							"<p> by " + reply[item].initiator + "</p>" +
+							"<h4><p>Start point: " + reply[item].start_point + "<br> End point: " + reply[item].end_point + "</p></h4>"
+							"<h4><p>Start Time: " + reply[item].start_time + "<br> End Time: " + reply[item].end_time + "</p> </h4>" +
+							"<span> " + reply[item].service_desc + "</span>" +
+							"<button >Already a member</button>"
+							+ "</div> </li>";
+						}
+
+						else{
+							{#here, reply[item] is our service object, can access fields directly,#}
+							select.innerHTML += "<li><div>" +
+							"<h4>Shopping Service</h4>" +
+							"<h5>" + reply[item].event_type + "</h5>" +
+							"<h4><p>Start point: " + reply[item].start_point + "<br> End point: " + reply[item].end_point + "</p></h4>"
+							"<p> by " + reply[item].initiator + "</p>" +
+							"<h4><p>Start Time: " + reply[item].start_time + "<br> End Time: " + reply[item].end_time + "</p> </h4>" +
+							"<span> " + reply[item].service_desc + "</span>" +
+							"<button onclick=\"join_service(" + reply[item].service_id + ")\" id=\"" + reply[item].service_id + "\">Join Service</button>"
+							+ "</div> </li>";
+						}
+					}
+					
+				}
+			}
+
+		}).catch(function(error){
+			//handle error
+			alert(error);
+			console.log(error); 
+		})
+}
+
+
+function join_service(service_id){
+	var btn=getElementsById(service_id);
+
+	if(btn.innerHTML=='Already a member'){
+		alert("You are already a member of this service")
+	}
+
+	if(btn.innerHTML=='Join Service'){
+	axios.put('/api/addservicemember/',{
+		params:{
+			id=service_id,
+		}
+	}).then(
+		function(response){
+			reply=response.data;
+			console.log(reply);
+			if(reply.message=='Successfully joined the service'){
+				btn.innerHTML="Already a member";
+			}
+
+			else{
+				btn.innerHTML="Service ended"
+			}
+		})
+	}
+}
